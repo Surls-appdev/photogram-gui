c1d72d8609279eb651f24d86c31113c4064cdb2b
Matt surls <msurls@ChicagoBooth.edu>
2023-04-30 21:37:28 +0000

    Completed update user. All done. 100.0%

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,10 +29,30 @@ class UsersController < ApplicationController
 
     new_user.save
 
-    next_url = "/users/"
+    next_url = "/users/" + new_user.username.to_s
 
     redirect_to(next_url)
     #render({ :template => "user_templates/add_user.html.erb"})
+  end
+
+  def update_user
+    # Parameters: {"query_username"=>"MikeHe", "path_username"=>"Mikey"}
+    orig_username = params.fetch("path_username")
+    new_username = params.fetch("query_username")
+    
+    matching_user = User.where({ :username => orig_username})
+
+    the_user = matching_user.at(0)
+
+    the_user.username = new_username
+
+    the_user.save
+
+    #render({ :template => "user_templates/update_user.html.erb" })
+
+    next_url = "/users/" + the_user.username.to_s
+
+    redirect_to(next_url)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get("/users/:path_username", { :controller => "users", :action => "show"})
   get("/add_user", { :controller => "users", :action => "add_user"})
+  get("/update_user/:path_username", { :controller => "users", :action => "update_user"})
 
 
   get("/photos", { :controller => "photos", :action => "index"})
